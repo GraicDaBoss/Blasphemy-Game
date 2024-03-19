e3df3c585145e441eb9e36a3b0f8d613d51b2a38
GraicDaBoss <145370588+GraicDaBoss@users.noreply.github.com>
2024-03-19 15:04:05 +0000

    RESPAWN YUP

--- BLASPHEMY/Assets/Scripts/PlayerHealth.cs
+++ BLASPHEMY/Assets/Scripts/PlayerHealth.cs
@@ -9,18 +9,40 @@ public class PlayerHealth : MonoBehaviour
     public float maxHealth;
     public Image healthBar;
     public int damage = 10;
+    public float farts;
+
+
+    
+
 
     void Start()
     {
         maxHealth = health;
     }
 
+    IEnumerator Respawn()
+    {
+        health = 100;
+        GetComponent<PlayerMovement>().enabled = false;
+        transform.position = new Vector3(1, 1, 0);
+        yield return new WaitForSeconds(1);
+        GetComponent<PlayerMovement>().enabled = true;
+    }
+
     void Update()
     {
         healthBar.fillAmount = Mathf.Clamp(health / maxHealth, 0, 1);
 
+        if (health <= 0)
+        {
+            StartCoroutine(Respawn());
+
+            print("DEAD");
+        }
         
-     
+      
+
+
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -29,5 +51,7 @@ public class PlayerHealth : MonoBehaviour
             health -= damage;
             Debug.Log("Player health decreased by: " + damage);
         }
+
+   
     }
 }

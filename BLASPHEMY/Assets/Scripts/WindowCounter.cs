63a2c0f0a8203c499449243a5913563e2e1a55dd
GraicDaBoss <145370588+GraicDaBoss@users.noreply.github.com>
2024-03-20 15:07:51 +0000

    yuh

--- BLASPHEMY/Assets/Scripts/WindowCounter.cs
+++ BLASPHEMY/Assets/Scripts/WindowCounter.cs
@@ -2,16 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class WindowCounter : MonoBehaviour
 {
     public static WindowCounter instance;
-
+    public Image healthBar2;
     public TMP_Text WindowText;
     public int currentwindows = 0;
     public int totalWindows = 3;
+    public int windowHealth = 0;
     public GameObject objectToActivate;
     public GameObject objectToActivate2;
+    
     void Awake()
     {
         instance = this;
@@ -19,14 +22,19 @@ public class WindowCounter : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        currentwindows = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        healthBar2.fillAmount = Mathf.Clamp(currentwindows / totalWindows, 0, 1);
+
+        
 
     }
+
+
     public void IncreaseWindows(int v)
     {
         currentwindows += v;
